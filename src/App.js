61d3ef021aeae904c5b7e8ce4b50d9f8263a5277
Shubhangi-0 <shubhangishinde875@gmail.com>
2024-06-10 18:51:02 +0530

    intialize

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const email=e.target.email.value;
+    const password=e.target.password.value;
+    console.log("Enter your email :" +email,"\n","Enter your password : " +password);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div class="color">
+    <form onSubmit={handleSubmit}>
+          <div class="App">
+             <div class='Login'>
+                <div class='LoginBox'>
+                  <div class='LoginHeader'>
+                    Login
+                  </div>
+                    <div class="input">
+                      <input class="email" name="email" placeholder="Enter your email"/><br></br>
+                      <input class="password"name="password" placeholder="Enter your password" type="password"/>
+                    </div>
+                  <div style={{display:"flex",justifyContent:"center"}}>
+                  <button class="submitbutton">Login</button>
+                   </div>
+                </div>
+             </div></div>
+      </form>
+  </div>
   );
 }
-
 export default App;
---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(){
+    return (
+        <div className='Login'>
+            <div className='LoginBox'>
+                <div className='LoginHeader'>
+                    Login
+                </div>
+            </div>
+        </div>
+    )
+}
